cd4ccf5424e1ca23b8ecbaf87fd7230430a01f04
Lucas McDaniel <lmcdaniel@myemma.com>
2018-11-02 13:15:28 -0500

    Added custom route, extended a template, and added template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It woks!');
+  res.render('hello', {
+    name: 'Lucas',
+    dog: req.query.dog,
+  });
 });
 
 module.exports = router;
