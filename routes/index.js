0dce624559dd96c6859cbbf71602fd25b6331cff
Lucas McDaniel <lmcdaniel@myemma.com>
2018-12-20 13:49:31 -0600

    Added login and logout capabilities

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -36,6 +37,8 @@ router.post('/register',
   userController.validateRegister,
   userController.register,
   authController.login,
-  )
+);
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
