2698777280bc50f16788319bd44c76b4b470ebe1
Lucas McDaniel <lmcdaniel@myemma.com>
2018-12-20 14:02:09 -0600

    Login is now mandatory to add a store

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,4 +11,14 @@ exports.logout = (req, res) => {
   req.logout();
   req.flash('success', 'You are now logged out!');
   res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  // check if the user is authenticated
+  if (req.isAuthenticated()) {
+    next(); // they are logged in, so carry on
+    return;
+  }
+  req.flash('error', 'Oops! You must be logged in to do that.');
+  res.redirect('/login');
 }
